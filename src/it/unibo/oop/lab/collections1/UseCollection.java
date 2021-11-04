88bbb4a63f2021967054d0d9c5859722c072b355
Daniel Pellanda <maurizio.pellanda@studio.unibo.it>
2021-11-04 21:27:18 +0100

    Esercise 1 and 2 completed

--- src/it/unibo/oop/lab/collections1/UseCollection.java
+++ src/it/unibo/oop/lab/collections1/UseCollection.java
@@ -1,11 +1,13 @@
 package it.unibo.oop.lab.collections1;
 
+import java.util.*;
+
 /**
  * Example class using {@link java.util.List} and {@link java.util.Map}.
  * 
  */
 public final class UseCollection {
-
+	
     private UseCollection() {
     }
 
@@ -13,6 +15,15 @@ public final class UseCollection {
      * @param s
      *            unused
      */
+    
+    private static final int ELEMENTS_ADDED_HEAD = 100_000;
+    private static final int ELEMENTS_ADDED_MIDDLE = 1_000;
+    
+    private static void calculatePerformance(long time, String subject) {
+    	time = System.nanoTime() - time;
+        System.out.println(subject + " took " + ((double)time) / 1_000_000 +"ms to process.");
+    }
+    
     public static void main(final String... s) {
         /*
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
@@ -63,5 +74,80 @@ public final class UseCollection {
         /*
          * 8) Compute the population of the world
          */
+    	
+    	Map<String, Long> map = new HashMap<String, Long>();
+    	List<Integer> array = new ArrayList<Integer>();
+    	List<Integer> linked;
+    	
+    	for(int i = 1000; i <= 2000; i++) {
+    		array.add(i);
+    	}
+    	linked = new LinkedList<Integer>(array);
+    	
+
+    	int temp = linked.get(linked.size() - 1);
+    	linked.set(linked.size() - 1, linked.get(0));
+    	linked.set(0, temp);
+    	
+    	System.out.println("ArrayList :");
+    	for(int num : array) {
+    		System.out.println(num);
+    	}
+    	System.out.println();
+    	
+    	
+    	long time = System.nanoTime();
+    	for(int i = 0; i < ELEMENTS_ADDED_HEAD; i++) {
+    		array.add(0, (int)(Math.random() * 100));
+    	}
+    	calculatePerformance(time, "ArrayList head insertion");
+    	time = System.nanoTime();
+    	for(int i = 0; i < ELEMENTS_ADDED_HEAD; i++) {
+    		linked.add(0, (int)(Math.random() * 100));
+    	}
+    	calculatePerformance(time, "LinkedList head insertion");
+    	
+    	time = System.nanoTime();
+    	for(int i = 0; i < ELEMENTS_ADDED_MIDDLE; i++) {
+    		array.add(array.size() / 2, (int)(Math.random() * 100));
+    	}
+    	calculatePerformance(time, "ArrayList head insertion");
+    	time = System.nanoTime();
+    	for(int i = 0; i < ELEMENTS_ADDED_MIDDLE; i++) {
+    		linked.add(linked.size() / 2, (int)(Math.random() * 100));
+    	}
+    	calculatePerformance(time, "LinkedList head insertion");
+    	System.out.println();
+    	
+    	/*
+         * 7) Build a new Map that associates to each continent's name its
+         * population:
+         * 
+         * Africa -> 1,110,635,000
+         * 
+         * Americas -> 972,005,000
+         * 
+         * Antarctica -> 0
+         * 
+         * Asia -> 4,298,723,000
+         * 
+         * Europe -> 742,452,000
+         * 
+         * Oceania -> 38,304,000
+         */
+    	
+    	map.put("Africa", 1_110_635_000L);
+    	map.put("Americas", 972_005_000L);
+    	map.put("Antarctica", 0L);
+    	map.put("Asia", 4_298_723_000L);
+    	map.put("Europe", 742_452_000L);
+    	map.put("Oceania", 38_304_000L);
+    	
+    	long population = 0;
+    	for(long n : map.values()) {
+    		population += n;
+    	}
+    	System.out.println("World population is: "+ population);
+    	
     }
 }
